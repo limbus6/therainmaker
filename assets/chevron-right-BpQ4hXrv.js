c4eb7dd08b7cd408b4914ac75bd0f5d2b7689cba
limbus6 <limbus6@gmail.com>
2026-05-18 16:03:58 +0100

    Updates

--- assets/chevron-right-BpQ4hXrv.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./createLucideIcon-Cc7CxCer.js";var t=e(`chevron-right`,[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]);export{t};
---
+++ assets/chevron-right-ctBJpvUA.js
@@ -0,0 +1,1 @@
+import{t as e}from"./createLucideIcon-Bz6d6tdg.js";var t=e(`chevron-right`,[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]);export{t};
